ae4a8993d74007daa78766d8dcd6e3e770b69e37
Roman-Zajic <zajic.roman.mobile@gmail.com>
2021-10-10 10:22:02 +0200

    working example

---
+++ pricing.py
@@ -0,0 +1,104 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import tensorflow as tf
+
+from tensorflow.keras import layers
+from tensorflow.keras.layers.experimental import preprocessing
+
+np.set_printoptions(precision=3, suppress=True)
+
+
+def plot_horsepower(x, y):
+    plt.scatter(train_features['Horsepower'], train_labels, label='Data')
+    plt.plot(x, y, color='k', label='Predictions')
+    plt.xlabel('Horsepower')
+    plt.ylabel('MPG')
+    plt.legend()
+    plt.show()
+
+
+print(tf.__version__)
+
+url = 'C:\Users\zajic\PycharmProjects\pricing/auto-mpg.data'
+column_names = ['MPG', 'Cylinders', 'Displacement', 'Horsepower', 'Weight', 'Acceleration', 'Model Year', 'Origin']
+
+raw_dataset = pd.read_csv(url, names=column_names, na_values='?', comment='\t', sep=' ', skipinitialspace=True)
+
+dataset = raw_dataset.copy()
+dataset.tail()
+dataset.isna().sum()
+dataset = dataset.dropna()
+
+# one-hot encoding
+dataset['Origin'] = dataset['Origin'].map({1: 'USA', 2: 'Europe', 3: 'Japan'})
+
+dataset = pd.get_dummies(dataset, columns=['Origin'], prefix='', prefix_sep='')
+dataset.tail()
+
+train_dataset = dataset.sample(frac=0.8, random_state=0)
+test_dataset = dataset.drop(train_dataset.index)
+
+sns.pairplot(train_dataset[['MPG', 'Cylinders', 'Displacement', 'Weight']], diag_kind='kde')
+plt.show()
+
+train_dataset.describe().transpose()
+
+train_features = train_dataset.copy()
+test_features = test_dataset.copy()
+
+train_labels = train_features.pop('MPG')
+test_labels = test_features.pop('MPG')
+
+train_dataset.describe().transpose()[['mean', 'std']]
+
+normalizer = preprocessing.Normalization(axis=-1)
+
+normalizer.adapt(np.array(train_features))
+
+print(normalizer.mean.numpy())
+
+first = np.array(train_features[:1])
+
+with np.printoptions(precision=2, suppress=True):
+    print('First example:', first)
+    print()
+    print('Normalized:', normalizer(first).numpy())
+
+horsepower = np.array(train_features['Horsepower'])
+
+horsepower_normalizer = preprocessing.Normalization(input_shape=[1, ], axis=None)
+horsepower_normalizer.adapt(horsepower)
+
+horsepower_model = tf.keras.Sequential([
+    horsepower_normalizer,
+    layers.Dense(units=1)
+])
+
+horsepower_model.summary()
+
+horsepower_model.predict(horsepower[:10])
+
+horsepower_model.compile(
+    optimizer=tf.optimizers.Adam(learning_rate=0.1),
+    loss='mean_absolute_error')
+
+history = horsepower_model.fit(
+    train_features['Horsepower'], train_labels,
+    epochs=100,
+    # suppress logging
+    verbose=1,
+    # Calculate validation results on 20% of the training data
+    validation_split=0.2)
+
+test_results = {'horsepower_model': horsepower_model.evaluate(
+    test_features['Horsepower'],
+    test_labels, verbose=0)}
+
+x = tf.linspace(0.0, 250, 251)
+y = horsepower_model.predict(x)
+
+plot_horsepower(x, y)
+
+print('ppp')
